9c65c367b35fd8dfda64c962e51f066a5a982459
al-lek <tmp@tmp.com>
2019-10-04 19:46:24 +0300

    Add project files.

---
+++ Json Socket Client/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Json_Socket_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.conn_btn = new System.Windows.Forms.Button();
+            this.set_btn = new System.Windows.Forms.Button();
+            this.check_btn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.init_btn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.calib_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // conn_btn
+            // 
+            this.conn_btn.Location = new System.Drawing.Point(37, 39);
+            this.conn_btn.Name = "conn_btn";
+            this.conn_btn.Size = new System.Drawing.Size(75, 23);
+            this.conn_btn.TabIndex = 0;
+            this.conn_btn.Text = "Connect";
+            this.conn_btn.UseVisualStyleBackColor = true;
+            // 
+            // set_btn
+            // 
+            this.set_btn.Location = new System.Drawing.Point(37, 68);
+            this.set_btn.Name = "set_btn";
+            this.set_btn.Size = new System.Drawing.Size(75, 23);
+            this.set_btn.TabIndex = 0;
+            this.set_btn.Text = "send";
+            this.set_btn.UseVisualStyleBackColor = true;
+            // 
+            // check_btn
+            // 
+            this.check_btn.Location = new System.Drawing.Point(37, 97);
+            this.check_btn.Name = "check_btn";
+            this.check_btn.Size = new System.Drawing.Size(75, 23);
+            this.check_btn.TabIndex = 0;
+            this.check_btn.Text = "check";
+            this.check_btn.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(141, 39);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(237, 326);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // init_btn
+            // 
+            this.init_btn.Location = new System.Drawing.Point(37, 126);
+            this.init_btn.Name = "init_btn";
+            this.init_btn.Size = new System.Drawing.Size(75, 23);
+            this.init_btn.TabIndex = 0;
+            this.init_btn.Text = "init";
+            this.init_btn.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // calib_btn
+            // 
+            this.calib_btn.Location = new System.Drawing.Point(37, 155);
+            this.calib_btn.Name = "calib_btn";
+            this.calib_btn.Size = new System.Drawing.Size(75, 23);
+            this.calib_btn.TabIndex = 0;
+            this.calib_btn.Text = "calibrate";
+            this.calib_btn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 389);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.calib_btn);
+            this.Controls.Add(this.init_btn);
+            this.Controls.Add(this.check_btn);
+            this.Controls.Add(this.set_btn);
+            this.Controls.Add(this.conn_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button conn_btn;
+        private System.Windows.Forms.Button set_btn;
+        private System.Windows.Forms.Button check_btn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button init_btn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button calib_btn;
+    }
+}
+
---
+++ Json Socket Client/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Json_Socket_Client
+{
+    public partial class Form1 : Form
+    {
+        Socket socket;
+        int[] atto_mVolt = new int[] { 30000, 30000, 30000 };
+        int[] atto_mVoltDC = new int[] { 0, 0, 0 };
+        int[] atto_mHz = new int[] { 1001000, 1001000, 1001000 };
+
+        List<string> commands = new List<string> {
+            //"com.attocube.amc.description.getPositionersList",
+            //"com.attocube.amc.description.getControlAmplitude",
+
+            "com.attocube.amc.status.getStatusConnected",       // axis
+            "com.attocube.amc.status.getStatusMoving",
+            "com.attocube.amc.status.getStatusReference",
+
+            "com.attocube.amc.control.getActorName",
+            "com.attocube.amc.control.getActorType",
+
+            //"com.attocube.amc.control.getActorParameters",
+            "com.attocube.amc.control.getControlAmplitude",
+            "com.attocube.amc.control.getControlFrequency",
+
+            "com.attocube.amc.move.getPosition",
+            "com.attocube.amc.control.getReferencePosition",
+
+            "com.attocube.amc.move.getNSteps",
+            "com.attocube.amc.move.getControlTargetPosition",
+
+            "com.attocube.amc.move.getControlEotOutputDeactive",
+            "com.attocube.amc.control.getControlOutput",
+
+            "com.attocube.amc.control.getControlMove",
+            "com.attocube.amc.move.getControlContinousFwd",
+            "com.attocube.amc.move.getControlContinousBkwd",
+
+            "com.attocube.amc.control.getControlReferenceAutoUpdate",
+            "com.attocube.amc.control.getControlAutoReset",
+            "com.attocube.amc.control.getControlTargetRange",
+            "com.attocube.amc.control.getControlFixOutputVoltage",
+
+            "com.attocube.amc.rtin.getControlMoveGPIO"
+            };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            conn_btn.Click += (s, e) =>
+            {
+                Button btn = (Button)s;
+                if (btn.Text == "Connect") { atto_conn(true); btn.Text = "Disconnect"; }
+                else { atto_conn(false); btn.Text = "Connect"; }
+            };
+            set_btn.Click += (s, e) => { send(); };
+            check_btn.Click += (s, e) => { atto_check(); };
+            init_btn.Click += (s, e) => { atto_init(); };
+            calib_btn.Click += (s, e) => { atto_calibrate(); };
+        }
+
+        #region high level atto comm
+        private void atto_conn(bool connect)
+        {
+            if (connect)
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect("192.168.1.1", 9090);
+
+                if (socket.Connected) richTextBox1.AppendText("connected..." + "\n");
+                else richTextBox1.AppendText("connection fail..." + "\n");
+            }
+            else
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+
+                if (socket.Connected) richTextBox1.AppendText("still connected..." + "\n");
+                else richTextBox1.AppendText("disconnected..." + "\n");
+            }
+        }
+
+        private bool atto_check()
+        {
+            // ensure all actuators are up
+            string comm_check = "com.attocube.amc.status.getStatusConnected";
+            bool ok = true;
+
+            for (int i = 0; i < 3; i++)
+                if (!str_to_bool(transmit_receive(comm_check, 0, new int[] { i })[1]))
+                    ok = false;
+
+            return ok;
+        }
+
+        private bool atto_init()
+        {
+            // initialize params for all actuators. volt, dc level, frequency
+            string mvolt_init = "com.attocube.amc.control.setControlAmplitude";
+            //string mvoltDC_init = "com.attocube.amc.control.setControlFixOutputVoltage";
+            string mHz_init = "com.attocube.amc.control.setControlFrequency";
+            bool ok = true;
+
+            for (int i = 0; i < 3; i++)
+                if (str_to_int(transmit_receive(mvolt_init, 0, new int[] { i, atto_mVolt[i] })[0]) != 0)
+                    ok = false;
+
+            //for (int i = 0; i < 3; i++)
+            //    if (str_to_int(transmit_receive(mvoltDC_init, 0, new int[] { i, atto_mVoltDC[i] })[0]) != 0)
+            //        ok = false;
+
+            for (int i = 0; i < 3; i++)
+                if (str_to_int(transmit_receive(mHz_init, 0, new int[] { i, atto_mHz[i] })[0]) != 0)
+                    ok = false;
+
+            return ok;
+        }
+
+        private bool atto_calibrate()
+        {
+            // routine to find absolute positions
+
+
+            return true;
+        }
+        #endregion
+
+        private void send()
+        {
+            int idx = Convert.ToInt32(textBox1.Text);
+            int[] axis = new int[] { 2 };
+
+            string[] response = transmit_receive(commands[idx], 0, axis);
+
+            if (response == null) return;
+
+            richTextBox1.AppendText(commands[idx] + "\n");
+            foreach (string str in response) richTextBox1.AppendText(str + "\t");
+            richTextBox1.AppendText("\n");
+        }
+
+
+        #region low Level atto comm
+        private string[] transmit_receive(string method, int id, int[] parameters = null)
+        {
+            // 1. transmit command
+            byte[] cmd_bytes = generate_command(method, id, parameters);
+            socket.Send(cmd_bytes);
+
+            // 2. get response
+            byte[] buffer = new byte[1024 * 4];
+            int readBytes = socket.Receive(buffer);
+            MemoryStream memoryStream = new MemoryStream();
+            while (readBytes > 0)
+            {
+                memoryStream.Write(buffer, 0, readBytes);
+                if (socket.Available > 0)
+                    readBytes = socket.Receive(buffer);
+                else break;
+            }
+            byte[] resp_bytes = memoryStream.ToArray();
+            memoryStream.Close();
+
+            // 3. resolve
+            string[] response = resolve_response(resp_bytes);
+            if (response == null) MessageBox.Show("Error!!!");
+            return response;
+        }
+
+        private byte[] generate_command(string methods, int id, int[] parameters = null)
+        {
+            Command cmd = new Command { jsonrpc = "2.0", method = methods, @params = parameters, id = id };
+            string cmd_str = JsonConvert.SerializeObject(cmd);
+            byte[] cmd_bytes = Encoding.Default.GetBytes(cmd_str);
+            return cmd_bytes;
+        }
+
+        private string[] resolve_response(byte[] resp_bytes)
+        {
+            string resp_str = Encoding.Default.GetString(resp_bytes);
+            Response response = JsonConvert.DeserializeObject<Response>(resp_str);
+            return response.result;
+        }
+
+        public class Command
+        {
+            public string jsonrpc { get; set; }
+            public string method { get; set; }
+            public int[] @params { get; set; }
+            public int id { get; set; }
+        }
+
+        public class Response
+        {
+            public string jsonrpc { get; set; }
+            public string[] result { get; set; }
+            public int id { get; set; }
+        }
+
+        #endregion
+
+
+        #region general helpers
+        private bool str_to_bool(string str)
+        {
+            return Convert.ToBoolean(str);
+        }
+
+        private int str_to_int(string str)
+        {
+            return Convert.ToInt32(str);
+        }
+
+        #endregion
+
+    }
+
+
+
+    #region Notes
+    //var command = new
+    //{
+    //    jsonrpc = "2.0",
+    //    method = methods,
+    //    @params = parameters,
+    //    id = id
+    //};
+    //string jsonData = JsonConvert.SerializeObject(command);
+    //jsonData += "\n";
+    //richTextBox1.AppendText(jsonData + "\n");
+
+
+    #endregion
+}
